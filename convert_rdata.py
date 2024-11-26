6f3af99fd57a21dc0bafb34aeb3f92e128576790
olyssa <olyssa@mail.de>
2024-11-26 11:04:50 +0100

    updates

--- convert_rdata.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import rdata
-
-r_data = rdata.parser.parse_file("/Users/olyssa/Downloads/compGenomRData-1-2.0/inst/extdata/leukemiaExpressionSubset.rds")
-
-# %%
-as_dict = rdata.conversion.convert(r_data)
-df = pd.DataFrame(as_dict)
